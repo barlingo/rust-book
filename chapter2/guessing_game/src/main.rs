6eb1474a4b255a2753b55fabe5d5ac57b161c569
Diego Barranco <diegob@xps15.lan>
2022-05-02 20:35:06 -0400

    Added random generator numebr

--- chapter2/guessing_game/src/main.rs
+++ chapter2/guessing_game/src/main.rs
@@ -1,12 +1,21 @@
+extern crate rand;
+
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number: u16 = rand::thread_rng().gen_range(1, 101);
     println!("Please enter your guess:");
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("Could not read number.");
 
-    println!("You entered: {}", input);
+    // if secret_number == input {
+    //     println!("You guessed correctly!!");
+    // } else {
+    //     println!("You guessed incorrectly!!");
+    // }
+    println!("Secret Number: {}, Entered: {}", secret_number, input)
 }
