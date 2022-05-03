28db712524ed0ce958c724d4bd7f8dd33c974996
Diego Barranco <diegob@xps15.lan>
2022-05-02 21:27:28 -0400

    Added match statemnets

--- chapter2/guessing_game/src/main.rs
+++ chapter2/guessing_game/src/main.rs
@@ -1,21 +1,34 @@
 extern crate rand;
 
-use rand::Rng;
+use rand::{thread_rng, Rng};
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
-    let secret_number: u16 = rand::thread_rng().gen_range(1, 101);
-    println!("Please enter your guess:");
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Could not read number.");
+    let secret_number = thread_rng().gen_range(1, 101);
+    println!("Secret Number: {}", secret_number);
 
-    // if secret_number == input {
-    //     println!("You guessed correctly!!");
-    // } else {
-    //     println!("You guessed incorrectly!!");
-    // }
-    println!("Secret Number: {}, Entered: {}", secret_number, input)
+    loop {
+        println!("Please enter your guess [1-100]:");
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Could not read number.");
+
+        println!("{}", input);
+        let input: u32 = match &input.trim().parse() {
+            Ok(num) => *num,
+            Err(_) => continue,
+        };
+        match input.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
