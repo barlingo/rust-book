f219617cfe1e0b3ee651200f28f0482e57d97853
Diego Barranco <diegob@xps15.lan>
2022-05-05 22:22:48 -0400

    completed loops

--- chapter3/branches/src/main.rs
+++ chapter3/branches/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 7;
+    if number < 5 {
+        println!("Condition was true");
+    } else {
+        println!("Condition was false");
+    }
+    if number != 0 {
+        println!("number was something else than zero");
+    }
+
+    if number % 4 == 0 {
+        println!("divisible by 4");
+    } else if number % 3 == 0 {
+        println!("divisible by 3");
+    } else if number % 2 == 0 {
+        println!("divisible by 2");
+    } else {
+        println!("Not divisible by 2, 3, 4");
+    }
+
+    // if let
+    let condition = true;
+    let number = if condition { 5 } else { "six" };
 }
---
+++ chapter3/loops/src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+
+    println!("End count {}", count);
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("result count {}", result);
+
+    let mut number = 10;
+
+    while number != 0 {
+        println!("{}", number);
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+    let a = [10, 20, 30, 40, 50];
+    let mut idx = 0;
+    while idx < 5 {
+        println!("The value is {}", a[idx]);
+        idx += 1;
+    }
+
+    for number in (1..4).rev() {
+        println!("{}", number);
+    }
+    println!("LIFTOFF!!!");
+}
