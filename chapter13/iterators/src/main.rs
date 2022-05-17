22c67af554f22ca3851faa8ae640e8d968205224
Diego Barranco <diegob@xps15.lan>
2022-05-16 22:16:52 -0400

    iterators

---
+++ chapter13/iterators/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
